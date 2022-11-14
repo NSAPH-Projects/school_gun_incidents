074ec828478f156757b104913493c7a82d33ee57
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2022-11-14 09:35:42 -0500

    Move to old_code

--- code/old_code/make_discretized_datasets.R
+++ code/old_code/make_discretized_datasets.R
@@ -1,28 +1,28 @@
 source("code/helper_functions.R")
 
-confounders_names <- c("census_division_number", paste0(quantitative_confounders))
+all_confounder_names <- c("census_division_number", quantitative_confounders)
 decilized_confounder_names <- c("census_division_number", paste0(quantitative_confounders, "_decile"))
 decilized_confounder_names_incl_urban_rural <- c("urban_rural", decilized_confounder_names)
 decilized_confounder_names_state_level <- paste0(quantitative_confounders, "_decile")
 
 # to do: edit this function to allow more options/parameters like get_binary_exposure_continuous_confounders_dataset()
-get_undiscretized_dataset <- function(exposure_name){
+get_undiscretized_dataset <- function(exposure_name, confounder_names = NA){
   cleaned_data <-  fread("data/all_tracts_2020_subset_vars.csv")
-  if ("GEOID" %in% colnames(cleaned_data)){
-    data_analysis <- get_analysis_df(cleaned_data, exposure_name, c(all_confounder_names, "GEOID"))
+  if ("State_Name" %in% colnames(cleaned_data)){
+    data_analysis <- get_analysis_df(cleaned_data, exposure_name, c(all_confounder_names, "State_Name"))
   } else{
     data_analysis <- get_analysis_df(cleaned_data, exposure_name, all_confounder_names)
   }
-  data_analysis <- na.omit(data_analysis) # omit 784 Census tracts with no mental health index
+  data_analysis <- na.omit(data_analysis) 
   return(data_analysis)
 }
 
 get_binary_exposure_continuous_confounders_dataset <- function(exposure_name, write_file = F, cutoff = NA,
                                                                confounder_names = NA, alternate_data = NA, na_omit = T){
   if (is.na(alternate_data)){
-    df <-  fread("data/all_tracts_2020_subset_vars.csv")
+    df <- fread("data/all_tracts_2020_subset_vars.csv")
   } else{
-    df <-  fread(alternate_data)
+    df <- fread(alternate_data)
   }
   
   if (is.na(cutoff)){
@@ -42,17 +42,17 @@ get_binary_exposure_continuous_confounders_dataset <- function(exposure_name, wr
   }
   
   if (na_omit){
-    data_analysis <- na.omit(data_analysis) # omit 784 Census tracts with no mental health index
+    data_analysis <- na.omit(data_analysis)
   }
   
   return(data_analysis)
 }
 
-# discretize_treatment <- function(df){
-#   df <- copy(df)
-#   df$a <- ntile(df$a, 4)
-#   return(df)
-# }
+discretize_treatment <- function(df){
+   df <- copy(df)
+   df$a <- ntile(df$a, 4)
+   return(df)
+}
 
 decilize_quantitative_confounders <- function(df, write_file = F, decilized_confounders = NA){
   data_discretized <- copy(df)

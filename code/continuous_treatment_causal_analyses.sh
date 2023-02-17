d46c492036eda1c4bbeeb993c2fce567863aaf35
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-02-16 22:14:00 -0500

    Rename "associational_analysis.R" to "associational_analyses.R" etc

--- code/continuous_treatment_causal_analyses.sh
+++ code/continuous_treatment_causal_analyses.sh
@@ -1,4 +1,4 @@
-Rscript --vanilla continuous_treatment_causal_analysis.R -s 100 -sa 'state' -p '5.95' -t '95'
-Rscript --vanilla continuous_treatment_causal_analysis.R -s 100 -sa 'state' -p '1.99' -t '99'
-Rscript --vanilla continuous_treatment_causal_analysis.R -s 100 -sa 'state.urbanity' -p '5.95' -t '95'
-Rscript --vanilla continuous_treatment_causal_analysis.R -s 100 -sa 'state.urbanity' -p '1.99' -t '99'
+Rscript --vanilla continuous_treatment_causal_analyses.R -s 100 -sa 'state' -p '5.95' -t '95'
+Rscript --vanilla continuous_treatment_causal_analyses.R -s 100 -sa 'state' -p '1.99' -t '99'
+Rscript --vanilla continuous_treatment_causal_analyses.R -s 100 -sa 'state.urbanity' -p '5.95' -t '95'
+Rscript --vanilla continuous_treatment_causal_analyses.R -s 100 -sa 'state.urbanity' -p '1.99' -t '99'

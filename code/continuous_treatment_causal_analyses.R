0c63f0445738f27e765baf875270196eb85977ad
Michelle Qin <mqin@holy7c12409.rc.fas.harvard.edu>
2023-04-19 03:48:20 +0000

    correct typo in saving main analysis results

--- code/continuous_treatment_causal_analyses.R
+++ code/continuous_treatment_causal_analyses.R
@@ -82,7 +82,7 @@ results_match[["cov_bal.capped0.99"]] <- NULL
 results_match[["cov_bal.capped1"]] <- NULL
 
 # save results as txt file
-lapply(1:length(results),
+lapply(1:length(results_match),
        function(i) cat(paste(names(results_match)[i], results_match[[i]], collapse = ": "),
                        sep = "\n",
                        file=paste0(dir, "results/causal_analyses/", var_arg_a_p_match,".txt"),
@@ -118,7 +118,7 @@ results_weight[["cov_bal.capped0.99"]] <- NULL
 results_weight[["cov_bal.capped1"]] <- NULL
 
 # save results as txt file
-lapply(1:length(results),
+lapply(1:length(results_weight),
        function(i) cat(paste(names(results_weight)[i], results_weight[[i]], collapse = ": "),
                        sep = "\n",
                        file=paste0(dir, "results/causal_analyses/", var_arg_a_p_weight,".txt"),

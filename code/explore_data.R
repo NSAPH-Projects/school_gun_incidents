582294f0db7fce8c8510f236594e033cb9203f34
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-08-21 10:46:04 -0400

    Use same variables as prior analysis

--- code/explore_data.R
+++ code/explore_data.R
@@ -1,14 +1,16 @@
 print("## Load packages ----")
+
 library(data.table)
 library(ggplot2)
 
 
 print("## Read data ----")
+
 dir <- "../" # run code in the script location
 
-tracts_data <- fread(paste0(dir, "data/intermediate/intermediate_cleaned_data_aug2023.csv"))
+tracts_data <- fread(paste0(dir, "data/intermediate/intermediate_data_aug2023.csv"))
 
-qualitative_confounder_names <- "STATE_ABBR"
+qualitative_confounder_names <- c("census_division_number", "urban_rural")
 exposure_names <- c("dist_closest_dealer", "dist_closest_commercial")
 outcome_name <- "binary_shooting"
 quantitative_confounder_names <- colnames(tracts_data)[!(colnames(tracts_data) %in% c(qualitative_confounder_names,
@@ -19,6 +21,7 @@ quantitative_confounders_data <- subset(tracts_data, select = quantitative_confo
 
 
 print("## Calculate correlations between quantitative confounders ----")
+
 cor_matrix <- cor(as.matrix(quantitative_confounders_data))
 cor_matrix <- round(cor_matrix, 2)
 cor_matrix[upper.tri(cor_matrix, diag = T)] <- NA # set redundant values to NA; set the diagonal to NA too since it is equal to 1
@@ -32,55 +35,54 @@ cor_matrix_plot <- ggplot(cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.ticks = element_blank())
+cor_matrix_plot
 
-# Note that cor(population_per_100sqmi, hu_per_100_sqmi) = 0.96, cor(sports_MP33018a_B_P, sports_MP33017a_B_P) = 0.95
-# Therefore, exclude variables hu_per_100_sqmi, sports_MP33017a_B_P
+# Since cor(sports_MP33018a_B_P, sports_MP33017a_B_P) = 0.95, exclude sports_MP33017a_B_P
+
 
 print("## Remove highly correlated (>= 0.95) confounders ----")
 
-tracts_data_cleaned <- copy(tracts_data)
-quantitative_confounders_data_cleaned <- copy(quantitative_confounders_data)
-tracts_data_cleaned[, `:=`(hu_per_100_sqmi = NULL,
-                           sports_MP33017a_B_P = NULL)]
-quantitative_confounders_data_cleaned[, `:=`(hu_per_100_sqmi = NULL,
-                                             sports_MP33017a_B_P = NULL)]
-quantitative_confounder_names <- quantitative_confounder_names[!(quantitative_confounder_names %in% c("hu_per_100_sqmi",
-                                                                                                    "sports_MP33017a_B_P"))]
+tracts_data_final <- copy(tracts_data)
+quantitative_confounders_data_final <- copy(quantitative_confounders_data)
+tracts_data_final[, `:=`(sports_MP33017a_B_P = NULL)]
+quantitative_confounders_data_final[, `:=`(sports_MP33017a_B_P = NULL)]
+quantitative_confounder_names_final <- quantitative_confounder_names[!(quantitative_confounder_names %in% c("sports_MP33017a_B_P"))]
 
-# # recalculate correlation matrix after removing highly correlated (>= 0.95) confounders
-# cor_matrix_cleaned <- cor(as.matrix(quantitative_confounders_data_cleaned))
-# cor_matrix_cleaned <- round(cor_matrix_cleaned, 2)
-# cor_matrix_cleaned[upper.tri(cor_matrix_cleaned, diag = T)] <- NA # set redundant values to NA; set the diagonal to NA too since it is equal to 1
-# cor_matrix_cleaned <- melt(cor_matrix_cleaned, na.rm = T)
-# 
-# cor_matrix_cleaned_plot <- ggplot(cor_matrix_cleaned, aes(x = Var1, y = Var2, fill = value)) +
-#   geom_tile(color = "white") +
-#   scale_fill_gradient2(low = "red", high = "blue", limit = c(-1,1)) +
-#   geom_text(aes(Var1, Var2, label = value), color = "black") +
-#   theme(axis.text.x = element_text(angle = 45, hjust=1),
-#         axis.title.x = element_blank(),
-#         axis.title.y = element_blank(),
-#         axis.ticks = element_blank())
+# recalculate correlation matrix after removing highly correlated (>= 0.95) confounders
+cor_matrix_final <- cor(as.matrix(quantitative_confounders_data_final))
+cor_matrix_final <- round(cor_matrix_final, 2)
+cor_matrix_final[upper.tri(cor_matrix_final, diag = T)] <- NA # set redundant values to NA; set the diagonal to NA too since it is equal to 1
+cor_matrix_final <- melt(cor_matrix_final, na.rm = T)
+
+cor_matrix_final_plot <- ggplot(cor_matrix_final, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(low = "red", high = "blue", limit = c(-1,1)) +
+  geom_text(aes(Var1, Var2, label = value), color = "black") +
+  theme(axis.text.x = element_text(angle = 45, hjust=1),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks = element_blank())
+cor_matrix_final_plot
 
 
 print("## Plot distribution of dist_closest_dealer ----")
 
-summary(tracts_data_cleaned$dist_closest_dealer)
-hist(tracts_data_cleaned$dist_closest_dealer)
+summary(tracts_data_final$dist_closest_dealer)
+hist(tracts_data_final$dist_closest_dealer)
 
 
 print("## Plot distribution of dist_closest_commercial ----")
 
-summary(tracts_data_cleaned$dist_closest_commercial)
-hist(tracts_data_cleaned$dist_closest_commercial)
+summary(tracts_data_final$dist_closest_commercial)
+hist(tracts_data_final$dist_closest_commercial)
 
 
 print("## Plot distribution of quantitative confounders ----")
 
-for (var in quantitative_confounder_names){
+for (var in quantitative_confounder_names_final){
   print(var)
-  print(mean(tracts_data_cleaned[[var]]))
-  print(sd(tracts_data_cleaned[[var]]))
+  print(mean(tracts_data_final[[var]]))
+  print(sd(tracts_data_final[[var]]))
 }
 
 # [1] "P0010001"
@@ -89,24 +91,21 @@ for (var in quantitative_confounder_names){
 # [1] "populationtotals_DPOP_CY"
 # [1] 4280.126
 # [1] 3526.212
-# [1] "schools_per_100_sqmi"
+# [1] "hu_per_100_sqmi" ### different from previous result
+# [1] 191284.7
+# [1] 481055.6
+# [1] "schools_per_100_sqmi" ### different from previous result
 # [1] 0.02124115
 # [1] 0.05373203
-# [1] "area_sq_mile"
+# [1] "area_sq_mile" ### different from previous result
 # [1] 43.15273
 # [1] 227.4726
-# [1] "groupquarters_GQINST20_P"
+# [1] "groupquarters_GQINST20_P" ### different from previous result
 # [1] 25.13192
 # [1] 39.85418
 # [1] "prop_adult"
 # [1] 0.7783094
 # [1] 0.05433586
-# [1] "householdincome_MEDHINC_CY"
-# [1] 76648.12
-# [1] 36739.74
-# [1] "incomebyage_AVGIA15_CY"
-# [1] 60205.62
-# [1] 24904.1
 # [1] "householdincome_ACSSNAP_P"
 # [1] 12.35961
 # [1] 11.41292
@@ -140,7 +139,7 @@ for (var in quantitative_confounder_names){
 # [1] "educationalattainment_GRADDEG_CY_P"
 # [1] 13.02408
 # [1] 10.69426
-# [1] "firearm_retailers_per_100sqmi"
+# [1] "firearm_retailers_per_100sqmi" ### different from previous result
 # [1] 48.97875
 # [1] 107.4379
 # [1] "sports_MP33018a_B_P"
@@ -161,16 +160,9 @@ for (var in quantitative_confounder_names){
 # [1] "prop_hispanic"
 # [1] 0.1797497
 # [1] 0.2167559
-# [1] "num_schools"
-# [1] 2.121957
-# [1] 1.458394
-# [1] "num_dealers"
-# [1] 1.526851
-# [1] 2.056983
-# [1] "population_per_100sqmi"
-# [1] 459012.9
-# [1] 1045146
-# [1] "daytime_population_per_100sqmi"
-# [1] 459051.2
-# [1] 1342648
-
+# [1] "log_med_HH_income"
+# [1] 11.12088
+# [1] 0.7344127
+# [1] "log_avg_HH_income_15to24"
+# [1] 10.88946
+# [1] 0.95539
--- code/make_datasets.R
+++ code/make_datasets.R
@@ -1,15 +1,35 @@
 print("## Load packages ----")
+
 library(readxl)
 library(data.table)
 
 
 print("## Read data ----")
+
 dir <- "../" # run code in the script location
 
 tracts_data <- read_excel(paste0(dir, "data/input/private/gun_violence_v2.xlsx"))
 tracts_data <- as.data.table(tracts_data)
 # var_descriptions <- tracts_data[1, ] # save variable descriptions
 tracts_data <- tracts_data[2:nrow(tracts_data), ] # remove row of variable descriptions from dataset
+
+census_divisions_data <- fread(paste0(dir, "data/input/open/census_regions_divisions.csv"))
+
+urbanity_data <- fread(paste0(dir, "data/input/open/NCHSURCodes2013.csv"))
+
+
+print("## Merge Datasets ----")
+
+census_divisions_data <- subset(census_divisions_data, select = c("state_fips", "census_division_number"))
+census_divisions_data[, state_fips := ifelse(nchar(state_fips) == 1, paste0("0", state_fips), state_fips)] # add leading 0 if necessary, convert state_fips to character
+
+urbanity_data <- urbanity_data[, .(county_fips = as.character(`FIPS code`), urban_rural = `2013 code`)]
+urbanity_data[, county_fips := ifelse(nchar(county_fips) == 4, paste0("0", county_fips), county_fips)] # add leading 0 if necessary, convert state_fips to character
+
+tracts_data[, state_fips := substr(GEOID, 1, 2)]
+tracts_data[, county_fips := substr(GEOID, 1, 5)]
+tracts_data <- merge(tracts_data, census_divisions_data, by = "state_fips", all.x = T, all.y = F)
+tracts_data <- merge(tracts_data, urbanity_data, by = "county_fips", all.x = T, all.y = F)
 
 
 print("## Exclude some rows ----")
@@ -19,32 +39,43 @@ tracts_data <- tracts_data[P0010001 != 0] # remove 18 Census tracts with populat
 
 
 print("## Subset and transform variables ----")
+
 tracts_data[, GEOID := NULL]
+tracts_data[, state_fips := NULL]
+tracts_data[, county_fips := NULL]
+tracts_data[, STATE_ABBR := NULL]
 tracts_data[, groupquarters_GQNINST20_P := NULL] # collinear with groupquarters_GQINST20_P
+tracts_data[, num_dealers := NULL] # already including firearm_retailers_per_100sqmi
+tracts_data[, num_schools := NULL] # already including schools_per_100_sqmi
+tracts_data[, duration_weighted_density := NULL] # exclude this variable for simplicity
 tracts_data[, num_shootings := NULL] # not analyzing this outcome
 tracts_data[, incident_date := NULL] # not analyzing this outcome
 tracts_data[, dist_open_dealer := NULL] # not analyzing this exposure
 tracts_data[, dist_open_commercial := NULL] # not analyzing this exposure
-tracts_data[, duration_weighted_density := NULL] # exclude this variable for simplicity
 
-qualitative_confounder_names <- "STATE_ABBR"
+qualitative_confounder_names <- c("census_division_number", "urban_rural")
 
 for (var in colnames(tracts_data)){
-  if (!(var %in% qualitative_confounder_names)){ # alternatively, != "STATE_ABBR"
+  if (var %in% qualitative_confounder_names){
+    tracts_data[[var]] <- as.factor(tracts_data[[var]])
+  } else{
     tracts_data[[var]] <- as.numeric(tracts_data[[var]])
   }
 }
 
-tracts_data[, population_per_100sqmi := P0010001 / area_sq_mile * 100]
-tracts_data[, daytime_population_per_100sqmi := populationtotals_DPOP_CY / area_sq_mile * 100]
+tracts_data[, log_med_HH_income := log(householdincome_MEDHINC_CY + 0.01)]
+tracts_data[, log_avg_HH_income_15to24 := log(incomebyage_AVGIA15_CY + 0.01)]
+tracts_data[, householdincome_MEDHINC_CY := NULL]
+tracts_data[, incomebyage_AVGIA15_CY := NULL]
 
 
 print("## Remove highly correlated (>= 0.95) confounders ----")
 
-tracts_data_cleaned <- copy(tracts_data)
-tracts_data_cleaned[, `:=`(hu_per_100_sqmi = NULL,
-                           sports_MP33017a_B_P = NULL)]
+tracts_data_final <- copy(tracts_data)
+tracts_data_final[, `:=`(sports_MP33017a_B_P = NULL)]
+
 
 print("## Save intermediate and final datasets ----")
-fwrite(tracts_data, paste0(dir, "data/intermediate/intermediate_cleaned_data_aug2023.csv"))
-fwrite(tracts_data_cleaned, paste0(dir, "data/intermediate/cleaned_data_aug2023.csv"))
+
+fwrite(tracts_data, paste0(dir, "data/intermediate/intermediate_data_aug2023.csv"))
+fwrite(tracts_data_final, paste0(dir, "data/intermediate/final_data_aug2023.csv"))

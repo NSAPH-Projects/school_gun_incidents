f10edcd221f3aed607f16d6c0dc25336840c99c6
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-09-27 21:38:59 -0400

    Move code files

--- code/read_results.R
+++ code/read_results.R
@@ -19,43 +19,46 @@ causal_results_paths <- list.files(paste0(dir, "results/causal_analyses/"),
 
 read_one_associational_txt <- function(path){
   results <- fread(path, header = F)
-  results_formatted <- data.frame("Exposure" = results[1, 1],
-                                  "Model" = results[2, 1],
-                                  "Cat_Confounder" = results[3, 1],
+  results_formatted <- data.table("Exposure" = results[1, 1],
                                   "Trim" = results[4, 1],
+                                  "Cat_Confounder" = results[3, 1],
+                                  "Model" = results[2, 1],
                                   "Effect" = results[6, 1],
                                   "CI_lower" = results[8, 1],
                                   "CI_upper" = results[10, 1])
+  return(results_formatted)
 }
 
 read_one_causal_txt <- function(path){
   results <- fread(path, header = F, sep = " ", fill = T)
   if (results[2, 1] == "match"){ # if model is GPS matching with CRSE
-    results_formatted <- data.frame("Exposure" = results[1, 1],
-                                    "Model" = results[2, 1],
-                                    "Cat_Confounder" = results[3, 1],
+    results_formatted <- data.table("Exposure" = results[1, 1],
                                     "Trim" = results[4, 1],
+                                    "Cat_Confounder" = results[3, 1],
+                                    "Model" = results[2, 1],
                                     "Effect" = results[12, 1],
                                     "CI_lower" = results[14, 1],
                                     "CI_upper" = results[16, 1],
-                                    "Count/Weight_Max" = results[8, 1],
+                                    "Count/Weight_Max" = signif(as.numeric(results[8, 1]), digits = 4),
                                     "Count/Weight_Cap" = parse_number(as.character(results[10, 1])))
   } else{
-    results_formatted <- data.frame("Exposure" = results[1, 1],
-                                    "Model" = results[2, 1],
-                                    "Cat_Confounder" = results[3, 1],
+    results_formatted <- data.table("Exposure" = results[1, 1],
                                     "Trim" = results[4, 1],
+                                    "Cat_Confounder" = results[3, 1],
+                                    "Model" = results[2, 1],
                                     "Effect" = results[10, 1],
                                     "CI_lower" = results[12, 1],
                                     "CI_upper" = results[14, 1],
-                                    "Count/Weight_Max" = results[6, 1],
+                                    "Count/Weight_Max" = signif(as.numeric(results[6, 1]), digits = 4),
                                     "Count/Weight_Cap" = parse_number(as.character(results[8, 1])))
   }
+  return(results_formatted)
 }
 
 associational_results <- rbindlist(lapply(associational_results_paths, read_one_associational_txt))
-colnames(associational_results) <- c("Exposure", "Model", "Cat_Confounder", "Trim", "Effect", "CI_lower", "CI_upper")
+colnames(associational_results) <- c("Exposure", "Trim", "Cat_Confounder", "Model", "Effect", "CI_lower", "CI_upper")
+setorder(associational_results, Exposure, Trim, Cat_Confounder, Model)
 
 causal_results <- rbindlist(lapply(causal_results_paths, read_one_causal_txt))
-colnames(causal_results) <- c("Exposure", "Model", "Cat_Confounder", "Trim", "Effect", "CI_lower", "CI_upper", "Count/Weight_Max", "Count/Weight_Cap")
-
+colnames(causal_results) <- c("Exposure", "Trim", "Cat_Confounder", "Model", "Effect", "CI_lower", "CI_upper", "Count/Weight_Max", "Count/Weight_Cap")
+setorder(causal_results, Exposure, Trim, Cat_Confounder, Model)

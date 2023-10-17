97f5e615b1bcd8299937443b22890b4ec0279382
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-10-17 00:55:21 -0400

    Rename causal analyses file

--- code/continuous_treatment_causal_analyses.sh
+++
@@ -1,8 +0,0 @@
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_distance_all_persistent_dealers' -s 100 -sa 'state' -p '5.95'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_distance_all_persistent_dealers' -s 100 -sa 'state' -p '1.99'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_distance_all_persistent_dealers' -s 100 -sa 'state.urbanicity' -p '5.95'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_distance_all_persistent_dealers' -s 100 -sa 'state.urbanicity' -p '1.99'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_dist_commercial_dealers' -s 100 -sa 'state' -p '5.95'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_dist_commercial_dealers' -s 100 -sa 'state' -p '1.99'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_dist_commercial_dealers' -s 100 -sa 'state.urbanicity' -p '5.95'
-Rscript --vanilla continuous_treatment_causal_analyses.R -e 'mean_dist_commercial_dealers' -s 100 -sa 'state.urbanicity' -p '1.99'

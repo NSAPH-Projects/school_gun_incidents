ec676a8382c8879a15c4a7265b0aba63c26ac368
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-10-30 23:02:15 -0400

    sensitivity analyses for new data

--- code/supplementary/gee_associational_model.R
+++ code/supplementary/gee_associational_model.R
@@ -1,6 +1,5 @@
 ##### Note: GEE model may take up to 96 GB to run #####
 
-
 ## Load packages ----
 library(gee)
 library(MASS)
@@ -12,43 +11,38 @@ dir <- "../../" # repository folder; this file is in code/sensitivity_analyses
 source(paste0(dir, "lib/functions_to_load_data.R"))
 
 ## Load datasets ----
-df <- fread(paste0(dir, "data/intermediate/all_tracts_2020_subset_vars_revised.csv"))
+df <- fread(paste0(dir, "data/intermediate/final_data_sep2023.csv"))
+data_main_analysis <- get_analysis_df(df, "mean_dist_commercial_dealers", c("State_Name", "urbanicity", quantitative_covariates))
+exposure5.95 <- quantile(data_main_analysis$a, c(0.05, 0.95))
+data_trimmed <- data_main_analysis[data_main_analysis$a >= exposure5.95[1] &
+                                     data_main_analysis$a <= exposure5.95[2], ]
 
-## Main body ----
+## Check for spatial confounding ----
 
-# prepare dataset for main analysis
-data_with_state <- get_analysis_df(df, "mean_total_miles", c("State_Name", quantitative_covariates))
-
-# get 95th and 99th percentiles of exposure
-exposure1.99 <- quantile(data_with_state$a, c(0.01, 0.99))
-exposure5.95 <- quantile(data_with_state$a, c(0.05, 0.95))
-
-# get data including urban_rural variable
-data_with_urbanity_state <- get_analysis_df(df, "mean_total_miles", c("State_Name", "urban_rural", quantitative_covariates))
-
-## Check for spatial confounding
-
+# function to run GEE model, using state as cluster and exchangeability correlation structure
 gee_model <- function(df){
   data_contiguous_clusters <- df[order(df$State_Name), ]
   outcome <- gee(formula = y ~ .,
                  family = "binomial",
-                 data = data_contiguous_clusters[, c("y", "a", quantitative_covariates)],
+                 data = data_contiguous_clusters[, c("y", "a", "urbanicity", quantitative_covariates)],
                  id = data_contiguous_clusters$State_Name,
                  corstr = "exchangeable")
   return(summary(outcome)$coefficients)
 }
 
-state.5.95_gee <- gee_model(
-  data_with_state[data_with_state$a >= exposure5.95[1] & 
-                    data_with_state$a <= exposure5.95[2], ]
-)
+# run GEE model on main analysis (commercial exposure trimmed at 5th and 95th exposures)
+gee_results <- gee_model(data_trimmed)
 
-### get logistic regression results from GPS-matched pseudopopulations and save results as txt file
-cat(paste("GEE associational model, estimated odds:", round(exp(state.5.95_gee["a",]["Estimate"]), 4)),
-    paste("GEE associational model, 95% CI lower bound:", round(exp(state.5.95_gee["a",]["Estimate"] - 1.96 * state.5.95_gee["a",]["Robust S.E."]), 4)),
-    paste("GEE associational model, 95% CI upper bound:", round(exp(state.5.95_gee["a",]["Estimate"] + 1.96 * state.5.95_gee["a",]["Robust S.E."]), 4)),
-    paste("GEE associational model, 90% CI lower bound:", round(exp(state.5.95_gee["a",]["Estimate"] - 1.645 * state.5.95_gee["a",]["Robust S.E."]), 4)),
-    paste("GEE associational model, 90% CI upper bound:", round(exp(state.5.95_gee["a",]["Estimate"] + 1.645 * state.5.95_gee["a",]["Robust S.E."]), 4)),
-    sep = "\n",
-    file=paste0(dir, "results/sensitivity_analyses/gee_associational_model/gee_associational_model.txt"),
-    append=TRUE)
+# save results as csv file
+results_as_table <- data.table(Exposure = "mean_dist_commercial_dealers",
+                               Model = "GEE associational model",
+                               Cat_Confounder = "state.urbanicity",
+                               Trim = "5.95",
+                               Effect = round(exp(gee_results["a",]["Estimate"]), 4),
+                               CI_95ct_lower = round(exp(gee_results["a",]["Estimate"] - 1.96 * gee_results["a",]["Robust S.E."]), 4),
+                               CI_95ct_upper = round(exp(gee_results["a",]["Estimate"] + 1.96 * gee_results["a",]["Robust S.E."]), 4),
+                               CI_90ct_lower = round(exp(gee_results["a",]["Estimate"] - 1.645 * gee_results["a",]["Robust S.E."]), 4),
+                               CI_90ct_upper = round(exp(gee_results["a",]["Estimate"] + 1.645 * gee_results["a",]["Robust S.E."]), 4),
+                               Exposure_Unit = "Mile",
+                               Effect_Unit = "Odds")
+fwrite(results_as_table, file = paste0(dir, "results/sensitivity_analyses/gee_associational_model/gee_associational_model.csv"))
--- code/supplementary/matching_without_covariates_for_evalue.R
+++ code/supplementary/matching_without_covariates_for_evalue.R
@@ -27,13 +27,13 @@ source(paste0(dir, "lib/functions_using_gps.R"))
 
 ## Load datasets ----
 
-df <- fread(paste0(dir, "data/intermediate/all_tracts_2020_subset_vars_revised.csv"))
+df <- fread(paste0(dir, "data/intermediate/final_data_sep2023.csv"))
 
 
 ## Main body ----
 
 ## Get data, excluding classes of covariates ----
-data_without_covariate <- get_analysis_df(df, "mean_total_miles", c("State_Name", quantitative_covariates[!(quantitative_covariates %in% covariates_list[[args$covariate_name]])]))
+data_without_covariate <- get_analysis_df(df, "mean_dist_commercial_dealers", c("State_Name", "urbanicity", quantitative_covariates[!(quantitative_covariates %in% covariates_list[[args$covariate_name]])]))
 
 ## GPS matching ----
 
@@ -41,7 +41,7 @@ data_without_covariate <- get_analysis_df(df, "mean_total_miles", c("State_Name"
 match_without_covariate <- all_matching_results_1model(seed = 100,
                                                        data = data_without_covariate,
                                                        trim = c(0.05, 0.95),
-                                                       cat_covariate_names = "State_Name",
+                                                       cat_covariate_names = c("State_Name", "urbanicity"),
                                                        run_gee_model = F,
                                                        quant_covariates = quantitative_covariates[!(quantitative_covariates %in% covariates_list[[args$covariate_name]])])
 
@@ -49,9 +49,17 @@ match_without_covariate <- all_matching_results_1model(seed = 100,
 ggsave(paste0(dir, "results/sensitivity_analyses/e_value/", args$covariate_name, "_correlation_plot.png"),
        make_correlation_plot(match_without_covariate$cov_bal.capped0.99))
 
-# get logistic regression results from GPS-matched pseudopopulations and save results as txt file
-cat(paste("Excluded covariate:", args$covariate_name, "\n",
-          "Logistic regression estimated odds:", match_without_covariate$logistic_regression_estimated_odds),
-    sep = "\n",
-    file=paste0(dir, "results/sensitivity_analyses/e_value/gps_matching_without_", args$covariate_name,"_variables.txt"),
-    append=TRUE)
+# get logistic regression results from GPS-matched pseudopopulations and save results as csv file
+results_as_table <- data.table(Exposure = "mean_dist_commercial_dealers",
+                               Model = "Matching without covariates for e-value",
+                               Cat_Confounder = "state.urbanicity",
+                               Excluded_Confounder_Class = args$covariate_name,
+                               Trim = "5.95",
+                               Effect = match_without_covariate$logistic_regression_estimated_odds,
+                               CI_95ct_lower = NA,
+                               CI_95ct_upper = NA,
+                               CI_90ct_lower = NA,
+                               CI_90ct_upper = NA,
+                               Exposure_Unit = "Mile",
+                               Effect_Unit = "Odds")
+fwrite(results_as_table, file = paste0(dir, "results/sensitivity_analyses/e_value/gps_matching_without_", args$covariate_name, "_variables.csv"))

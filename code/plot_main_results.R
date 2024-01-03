2543189a82aecb273f629e699ede2229866aada0
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-01-03 14:39:12 -0500

    plot results as change in odds

--- code/plot_main_results.R
+++ code/plot_main_results.R
@@ -3,12 +3,18 @@
 library(data.table)
 
 
-## Read in main results ----
+## Get main results ----
 
 dir <- "../" # run code in the script location
 
+# read in main results
 df <- fread(file = paste0(dir, "results/commercial_dealers_association_and_causal_results.csv"))
 df <- df[Trim == "5.95" & Cat_Confounder == "state.urbanicity"] # get main models only
+
+# convert odds ratio to change (difference) in odds, as percent
+df[, `:=`(Effect_pct = round((1-Effect)*100, 2),
+          CI_95ct_lower_pct = round((1-CI_95ct_upper)*100, 2),
+          CI_95ct_upper_pct = round((1-CI_95ct_lower)*100, 2))]
 
 
 ## Main Plot ----
@@ -18,23 +24,22 @@ png(file = paste0(dir, "results/main_results_plot.png"),
     width = 7, height = 5, units = "in", res = 1200)
 
 # Create the plot
-plot(1:nrow(df), df$Effect, ylim = c(0.80, 1.15), 
-     xlab = "", ylab = "Odds Ratio", main = "",
+plot(1:nrow(df), df$Effect_pct, ylim = c(0, max(df$CI_95ct_upper_pct)), 
+     xlab = "", ylab = "Change in Odds of SGI (%)", main = "",
      xaxt = "n", yaxt = "n", pch = 16, cex = 1.5, col = "steelblue")
 
 # Add light grey grid
 grid(lty = "dotted", col = "lightgray")
 
 # Add error bars for the confidence intervals
-arrows(1:nrow(df), df$CI_95ct_lower, 1:nrow(df), df$CI_95ct_upper, 
+arrows(1:nrow(df), df$CI_95ct_lower_pct, 1:nrow(df), df$CI_95ct_upper_pct, 
        length = 0.05, angle = 90, code = 3, col = "steelblue")
 
 # Add model names as x-axis labels (horizontal and smaller)
 axis(1, at = 1:nrow(df), labels = df$Model, tick = FALSE, las = 1, cex.axis = 0.7)
-text(x = 1:nrow(df), y = -0.15, labels = df$Model, pos = 1, cex = 0.7, xpd = TRUE, adj = c(0.5, 0), col = "black")
 
 # Add horizontal line at the value of a null effect
-abline(h = 1, lty = 2, col = "black")
+abline(h = 0, lty = 2, col = "black")
 
 # Add Vertical line to divide the causal vs associational models
 abline(v = 3.5, lty = 2, col = "red")
@@ -46,7 +51,7 @@ mtext("(Regression Models)", side = 1, line = 3.5, at = 4.5, cex = 0.8)
 mtext("(Causal Models)", side = 1, line = 3.5, at = 2, cex = 0.8)
 
 # Make Y axis labels horizontal
-axis(2, las = 1, tcl = -0.5)
+axis(2, las = 1, tcl = -0.5) # to do: consider adding percent
 
 # Conclude
 dev.off()
